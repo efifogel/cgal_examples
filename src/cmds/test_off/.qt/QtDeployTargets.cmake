3c9d45cccfdf615b482e5f3553305e944de0881e
Efi Fogel <efifogel@gmail.com>
2025-08-17 08:33:10 +0300

    removed

--- src/cmds/test_off/.qt/QtDeployTargets.cmake
+++
@@ -1,4 +0,0 @@
-set(__QT_DEPLOY_TARGET_CGAL_Qt6_moc_and_resources_FILE /home/efif/trees/efif/src/cmds/tests/test_off/libCGAL_Qt6_moc_and_resources.a)
-set(__QT_DEPLOY_TARGET_CGAL_Qt6_moc_and_resources_TYPE STATIC_LIBRARY)
-set(__QT_DEPLOY_TARGET_test_off_FILE /home/efif/trees/efif/src/cmds/tests/test_off/test_off)
-set(__QT_DEPLOY_TARGET_test_off_TYPE EXECUTABLE)
