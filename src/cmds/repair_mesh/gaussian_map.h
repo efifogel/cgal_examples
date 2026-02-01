62fbde1a042629a9d1bf383a152b35abe33ea005
Efi Fogel <efifogel@gmail.com>
2026-02-01 11:37:29 +0200

    Added missing header

---
+++ src/cmds/repair_mesh/gaussian_map.h
@@ -0,0 +1,25 @@
+#ifndef GAUSSIAN_MAP_H
+#define GAUSSIAN_MAP_H
+
+#include <CGAL/Polygon_mesh_processing/compute_normal.h>
+
+#include "cgalex/merge_coplanar_faces.h"
+
+#include "build_gaussian_map.h"
+#include "Face_normal_map.h"
+#include "Vertex_flag_map.h"
+
+template <typename GaussianMap, typename Mesh, typename Kernel>
+GaussianMap gaussian_map(Mesh& mesh, const Kernel& kernel) {
+  GaussianMap gm;
+  auto np = CGAL::parameters::geom_traits(kernel);
+  Face_normal_map<Mesh> normals;
+  CGAL::Polygon_mesh_processing::compute_face_normals(mesh, normals, np);
+  merge_coplanar_faces(mesh, normals, np);
+  Vertex_flag_map<Mesh> vertex_flags;
+  auto npv = CGAL::parameters::vertex_index_map(vertex_flags);
+  build_gaussian_map(mesh, normals, gm, npv);
+  return gm;
+}
+
+#endif
